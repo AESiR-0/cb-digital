3026c7499fde1718080a9ca2a38766528fe9b98e
pratham <cyborgkiller1008@gmail.com>
2024-11-08 09:49:01 +0530

    Refactor Navbar component: wrap brand name in a Link and clean up code formatting

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const navItems = [
   { title: "our services", link: "/services" },
   { title: "our work", link: "/work" },
   { title: "team", link: "/team" },
- ];
+];
 
 interface nav {
   title: string;
@@ -30,7 +30,7 @@ export const Navbar = () => {
     <>
       <div className="w-screen items-center text-white  z-50  flex justify-between overflow-hidden px-5 md:px-20 h-20 bg-seconday-2">
         <div className="flex items-center justify-between w-full md:w-auto">
-          <span className="font-wig text-4xl">CB</span>
+          <Link href={'/'}><span className="font-wig text-4xl">CB</span></Link>
           <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
             <svg
               className="w-6 h-6"
@@ -53,11 +53,10 @@ export const Navbar = () => {
                 return (
                   <Link
                     key={index}
-                    className={`group hover:opacity-95  duration-200 ease-in-out hover:scale-95 font-clash font-black  transition-all ${
-                      item.link === pathName
+                    className={`group hover:opacity-95  duration-200 ease-in-out hover:scale-95 font-clash font-black  transition-all ${item.link === pathName
                         ? "text-accent"
                         : "text-secondary-4"
-                    }`}
+                      }`}
                     onClick={() => handleClick(item.link)}
                     href={""}
                   >
@@ -70,26 +69,23 @@ export const Navbar = () => {
           )}
         </div>
         <div
-          className={`hidden md:flex-row md:flex font-sans text-lg text-secondary-4 gap-10 ${
-            isOpen ? "flex" : "hidden"
-          } md:flex`}
+          className={`hidden md:flex-row md:flex font-sans text-lg text-secondary-4 gap-10 ${isOpen ? "flex" : "hidden"
+            } md:flex`}
         >
           {navItems.map((item: nav, index: number) => {
             return (
               <Link
                 key={index}
-                className={`group  hover:opacity-95  duration-200 ease-in-out hover:scale-95 font-semibold  ${
-                  item.link === pathName ? "text-accent" : "text-secondary-4"
-                }`}
+                className={`group  hover:opacity-95  duration-200 ease-in-out hover:scale-95 font-semibold  ${item.link === pathName ? "text-accent" : "text-secondary-4"
+                  }`}
                 href={item.link}
               >
                 {item.title}
                 <div
-                  className={`${
-                    item.link === pathName
+                  className={`${item.link === pathName
                       ? "bg-accent scale-x-100 group-hover:scale-x-0 "
                       : "scale-x-0 group-hover:scale-x-100 bg-accent "
-                  }  h-[2px] w-full  transition-all duration-300`}
+                    }  h-[2px] w-full  transition-all duration-300`}
                 ></div>
               </Link>
             );
